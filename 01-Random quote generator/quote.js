31a41c3b50d9e2176270271d59493852da9802e0
Ryan Colquhoun <ryan@kalhoon.com>
2017-02-11 16:39:45 -0500

    Array names generate randomly

--- 01-Random quote generator/quote.js
+++ 01-Random quote generator/quote.js
@@ -1,4 +1,4 @@
-function Person(name, quote, image) {
+/*function Person(name, quote, image) {
   this.name = name;
   this.quote = quote;
   this.image = '<img src="images01/' + image + '.jpg">';
@@ -20,34 +20,61 @@ var lukeSkywalker = new Person(
     "Luke Skywalker", 
     "May the force be with you", 
     "lukeSkywalker"
-);
+);*/
+
+// var randomNumber = Math.floor(Math.random() * 11) + 1;
+
+/*var quotes = ['Red', 'Blue', 'Green'];
+
+var divInfo = document.getElementById('info');
+var mainQuote = document.createElement('p');
+
+divInfo.appendChild(mainQuote);
+mainQuote.innerHTML = quotes[0];
 
 
-//var personImages = [Person.image];
 
+var quoteButton = document.getElementById('quoteButton');
+
+quoteButton.onclick = function () {
+    
+     for (var i = 0; i <= quotes.length; i++){
+          
+        
+        mainQuote.innerHTML = quotes[i];
+          divInfo.appendChild(mainQuote);
+   
+          //  var randomNumber = Math.floor(Math.random() * 11) + 1;
+           
+    }//closes onclick function
+
+}//closes for loop*/
+
+var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+var carNumber = cars.length - 1;
+var randomNumber = Math.floor(Math.random() * carNumber) + 1;
+var i = 0;
 
 
 var divInfo = document.getElementById('info');
 var mainQuote = document.createElement('p');
-//var messageText = 'Yo, Check it';
+
 
 divInfo.appendChild(mainQuote);
-mainQuote.innerHTML = lukeSkywalker.image;
+mainQuote.innerHTML = cars[0];
 
+//Button click that generates new value from cars array
 var quoteButton = document.getElementById('quoteButton');
-
-for (var i = 0; i <=25; i++){
+quoteButton.onclick = function () {
     
-    quoteButton.onclick = function() {
-            var randomNumber = Math.floor(Math.random() * 11) + 1;
-            mainQuote.innerHTML = randomNumber;
-            //divInfo.appendChild(mainQuote);
-    
-    }//closes onclick function
+   // if (i <= carNumber){
+    var randomNumber = Math.floor(Math.random() * carNumber) + 1;
+    mainQuote.innerHTML = cars[randomNumber];
+    //i++;
 
-}//closes for loop
+  //  } else if (i > carNumber){ 
+      //s  mainQuote.innerHTML = 'No more Cars bud!';
+   // }
+} // end of onclick function
 
-
-
- 
 
