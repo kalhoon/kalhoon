c7b3d492dceab731816a691bd5d03deea2d3e338
Ryan Colquhoun <ryan@kalhoon.com>
2017-02-11 21:08:03 -0500

    Quotes and names randomly change

--- 01-Random quote generator/quote.js
+++ 01-Random quote generator/quote.js
@@ -1,58 +1,29 @@
-/*function Person(name, quote, image) {
-  this.name = name;
-  this.quote = quote;
-  this.image = '<img src="images01/' + image + '.jpg">';
-}
-
-var darthVader = new Person(
-    "Darth Vader", 
-    "I find your lack of faith disturbing", 
-    "darthVader"
-);
-
-var emperor = new Person(
-    "Emperor Palpatine", 
-    "Not from a Jedi", 
-    "emperor"
-);
-
-var lukeSkywalker = new Person(
-    "Luke Skywalker", 
-    "May the force be with you", 
-    "lukeSkywalker"
-);*/
-
-// var randomNumber = Math.floor(Math.random() * 11) + 1;
-
-/*var quotes = ['Red', 'Blue', 'Green'];
-
-var divInfo = document.getElementById('info');
-var mainQuote = document.createElement('p');
-
-divInfo.appendChild(mainQuote);
-mainQuote.innerHTML = quotes[0];
-
-
-
-var quoteButton = document.getElementById('quoteButton');
-
-quoteButton.onclick = function () {
+var quotes = [
+    {phrase: 'I\'ll be back',
+     person: 'T-100',
+     movie: 'Terminator, 1984'},
     
-     for (var i = 0; i <= quotes.length; i++){
-          
-        
-        mainQuote.innerHTML = quotes[i];
-          divInfo.appendChild(mainQuote);
-   
-          //  var randomNumber = Math.floor(Math.random() * 11) + 1;
-           
-    }//closes onclick function
+    {phrase: 'Run! Get to the chopper!',
+     person: 'Dutch',
+     movie: 'Predator, 1987'},
+    
+    {phrase: 'See you at the party Richter!',
+     person: 'Douglas Quaid',
+     movie: 'Total Recall, 1990'},
+    
+    {phrase: 'Don\'t disturb my friend, he\'s dead tired',
+     person: 'John Matrix',
+     movie: 'Commando, 1985'},
+    
+    {phrase: 'Hasta la vista, baby',
+     person: 'T-100',
+     movie: 'Terminator 2, 1990'}
+    
+    ]
 
-}//closes for loop*/
-
-var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-var carNumber = cars.length - 1;
-var randomNumber = Math.floor(Math.random() * carNumber) + 1;
+//var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+var quoteNumber = quotes.length - 1;
+var randomNumber = Math.floor(Math.random() * quoteNumber) + 1;
 var i = 0;
 
 
@@ -61,15 +32,20 @@ var mainQuote = document.createElement('p');
 
 
 divInfo.appendChild(mainQuote);
-mainQuote.innerHTML = cars[0];
+mainQuote.innerHTML = quotes[randomNumber].phrase + '<br>';
+mainQuote.innerHTML += '- ' + quotes[randomNumber].person + ', ';
+mainQuote.innerHTML += quotes[randomNumber].movie;
 
 //Button click that generates new value from cars array
 var quoteButton = document.getElementById('quoteButton');
 quoteButton.onclick = function () {
     
    // if (i <= carNumber){
-    var randomNumber = Math.floor(Math.random() * carNumber) + 1;
-    mainQuote.innerHTML = cars[randomNumber];
+    var randomNumber = Math.floor(Math.random() * quoteNumber) + 1;
+    mainQuote.innerHTML = quotes[randomNumber].phrase + '<br>';
+    mainQuote.innerHTML += '- ' + quotes[randomNumber].person + ', ';
+    mainQuote.innerHTML += quotes[randomNumber].movie;
+    //mainQuote.innerHTML = cars[randomNumber];
     //i++;
 
   //  } else if (i > carNumber){ 
